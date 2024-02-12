f3fbdf4260da80084848e2b7101913cbfdf997b4
Oleksandr Soloviov <oleksolv@gmail.com>
2024-02-12 22:18:21 +0200

    feat(core): now lint is a part of plugin

--- playgrounds/mapplet-playground/src/main.tsx
+++ playgrounds/mapplet-playground/src/main.tsx
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './app/App';
+import MappletsOpenApi from '@mapples/mapplets-open-api';
+import env from '../env';
 
+MappletsOpenApi.init(env.baseURL);
 AppRegistry.registerComponent('MappletPlayground', () => App);

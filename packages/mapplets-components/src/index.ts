ac16a0cbea2d2ec54418cb67514c7c5c745b519a
Oleksandr Soloviov <oleksolv@gmail.com>
2024-02-15 09:36:54 +0200

    feat(mapplets-components): add media component

--- packages/mapplets-components/src/index.ts
+++ packages/mapplets-components/src/index.ts
@@ -2,21 +2,25 @@ import ButtonComponent from '@mapples/mapplets-button';
 import CardComponent from '@mapples/mapplets-card';
 import TextComponent from '@mapples/mapplets-text';
 import ViewComponent from '@mapples/mapplets-view';
+import MediaComponent from '@mapples/mapplets-media';
+
 import { merge } from 'lodash';
 
 const RenderComponentSet = merge(
   ButtonComponent,
   CardComponent,
   TextComponent,
-  ViewComponent
+  ViewComponent,
+  MediaComponent
 );
 
 export * from '@mapples/mapplets-button';
 export * from '@mapples/mapplets-card';
 export * from '@mapples/mapplets-text';
 export * from '@mapples/mapplets-view';
+export * from '@mapples/mapplets-media';
 
 export * from './types';
 
-export { ButtonComponent, CardComponent, TextComponent, ViewComponent };
+export { ButtonComponent, CardComponent, TextComponent, ViewComponent, MediaComponent };
 export default RenderComponentSet;

9990e703d7074e050d4ef99a277d49213730e13c
Oleksandr Soloviov <oleksolv@gmail.com>
2024-02-15 22:43:28 +0200

    fix(mapplets-media): add media component to render component type

--- packages/mapplets-components/src/types.ts
+++ packages/mapplets-components/src/types.ts
@@ -2,9 +2,11 @@ import { TextComponentType } from '@mapples/mapplets-text';
 import { ViewComponentType } from '@mapples/mapplets-view';
 import { CardComponentType } from '@mapples/mapplets-card';
 import { ButtonComponentType } from '@mapples/mapplets-button';
+import { MediaComponentType } from '@mapples/mapplets-media';
 
 export type RenderComponentType =
   | TextComponentType
   | ViewComponentType
   | CardComponentType
-  | ButtonComponentType;
+  | ButtonComponentType
+  | MediaComponentType;

b3473c9e3d8a37f209ba17e3eb5c901b9268fcb8
Oleksandr Soloviov <oleksolv@gmail.com>
2024-02-16 13:25:02 +0200

    fix(mapplets-renderer): add media to renderer

--- packages/mapplets-renderer/src/constants/index.ts
+++ packages/mapplets-renderer/src/constants/index.ts
@@ -4,10 +4,12 @@ import ButtonComponent from '@mapples/mapplets-button';
 import TextComponent from '@mapples/mapplets-text';
 import CardComponent from '@mapples/mapplets-card';
 import ViewComponent from '@mapples/mapplets-view';
+import MediaComponent from '@mapples/mapplets-media';
 
 export const RendererComponentSet = merge(
   ButtonComponent,
   TextComponent,
   CardComponent,
-  ViewComponent
+  ViewComponent,
+  MediaComponent
 );
--- packages/mapplets-renderer/src/types/index.ts
+++ packages/mapplets-renderer/src/types/index.ts
@@ -2,9 +2,11 @@ import { CardComponentType } from '@mapples/mapplets-card';
 import { ButtonComponentType } from '@mapples/mapplets-button';
 import { ViewComponentType } from '@mapples/mapplets-view';
 import { TextComponentType } from '@mapples/mapplets-text';
+import { MediaComponentType } from '@mapples/mapplets-media';
 
 export type RendererComponentType =
   | CardComponentType
   | ButtonComponentType
   | ViewComponentType
-  | TextComponentType;
+  | TextComponentType
+  | MediaComponentType;
